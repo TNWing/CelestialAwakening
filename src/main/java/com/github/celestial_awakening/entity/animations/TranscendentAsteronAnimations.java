d8008f8d8dcc0aab7ff91b0bfcf019b5b55fd80d
TNWing <trevngwing@gmail.com>
2025-02-17 05:12:02 -0500

    Updated Asteron Model/Anim

---
+++ src/main/java/com/github/celestial_awakening/entity/animations/TranscendentAsteronAnimations.java
@@ -0,0 +1,130 @@
+package com.github.celestial_awakening.entity.animations;// Save this class in your mod and generate all required imports
+
+import net.minecraft.client.animation.AnimationChannel;
+import net.minecraft.client.animation.AnimationDefinition;
+import net.minecraft.client.animation.Keyframe;
+import net.minecraft.client.animation.KeyframeAnimations;
+
+/**
+ * Made with Blockbench 4.12.2
+ * Exported for Minecraft version 1.19 or later with Mojang mappings
+ * @author Author
+ */
+public class TranscendentAsteronAnimations {
+	public static final AnimationDefinition idle = AnimationDefinition.Builder.withLength(0.0F)
+		
+		.build();
+
+	public static final AnimationDefinition basic_attack = AnimationDefinition.Builder.withLength(1.2083F)
+		.build();
+
+	public static final AnimationDefinition piercing_rays = AnimationDefinition.Builder.withLength(0.5F)
+		.addAnimation("root", new AnimationChannel(AnimationChannel.Targets.POSITION,
+			new Keyframe(0.0F, KeyframeAnimations.posVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.posVec(0.0F, -5.1F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("seg2bot", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
+			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 25.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("seg2bot", new AnimationChannel(AnimationChannel.Targets.POSITION, 
+			new Keyframe(0.0F, KeyframeAnimations.posVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.posVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("leg2_z", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
+			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.degreeVec(0.0F, 0.0F, -25.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("leg4_z", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
+			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.degreeVec(0.0F, 0.0F, -25.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("seg4bot", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
+			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 25.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("seg4bot", new AnimationChannel(AnimationChannel.Targets.POSITION, 
+			new Keyframe(0.0F, KeyframeAnimations.posVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.posVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("leg1_z", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
+			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.degreeVec(0.0F, 0.0F, -25.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("seg1bot", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
+			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 25.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("seg1bot", new AnimationChannel(AnimationChannel.Targets.POSITION, 
+			new Keyframe(0.0F, KeyframeAnimations.posVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.posVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("leg3_z", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
+			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.degreeVec(0.0F, 0.0F, -25.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("seg3bot", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
+			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 25.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("seg3bot", new AnimationChannel(AnimationChannel.Targets.POSITION, 
+			new Keyframe(0.0F, KeyframeAnimations.posVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.posVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.build();
+
+	public static final AnimationDefinition piercing_rays_recover = AnimationDefinition.Builder.withLength(0.5F)
+		.addAnimation("root", new AnimationChannel(AnimationChannel.Targets.POSITION, 
+			new Keyframe(0.0F, KeyframeAnimations.posVec(0.0F, -5.1F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.posVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("seg2bot", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
+			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 25.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("seg2bot", new AnimationChannel(AnimationChannel.Targets.POSITION, 
+			new Keyframe(0.0F, KeyframeAnimations.posVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.posVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("leg2_z", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
+			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, -25.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("leg4_z", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
+			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, -25.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("seg4bot", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
+			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 25.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("seg4bot", new AnimationChannel(AnimationChannel.Targets.POSITION, 
+			new Keyframe(0.0F, KeyframeAnimations.posVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.posVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("leg1_z", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
+			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, -25.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("seg1bot", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
+			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 25.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("seg1bot", new AnimationChannel(AnimationChannel.Targets.POSITION, 
+			new Keyframe(0.0F, KeyframeAnimations.posVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.posVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("leg3_z", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
+			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, -25.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("seg3bot", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
+			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 25.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("seg3bot", new AnimationChannel(AnimationChannel.Targets.POSITION, 
+			new Keyframe(0.0F, KeyframeAnimations.posVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.5F, KeyframeAnimations.posVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.build();
+}
--- src/main/java/com/github/celestial_awakening/rendering/client/models/TranscendentAsteronModel.java
+++ src/main/java/com/github/celestial_awakening/rendering/client/models/TranscendentAsteronModel.java
@@ -14,45 +14,52 @@ import net.minecraft.world.entity.Entity;
 public class TranscendentAsteronModel<T extends Entity> extends EntityModel<T> {
 	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
 	private final ModelPart root;
-	private final ModelPart face;
-	private final ModelPart leg1;
-	private final ModelPart seg1mid;
-	private final ModelPart seg1top;
-	private final ModelPart seg1bot;
-	private final ModelPart leg3;
-	private final ModelPart seg3mid;
-	private final ModelPart seg3top;
-	private final ModelPart seg3bot;
-	private final ModelPart leg4;
-	private final ModelPart seg4mid;
-	private final ModelPart seg4top;
-	private final ModelPart seg4bot;
-	private final ModelPart leg2;
+	private final ModelPart mainbody;
+	private final ModelPart leg2_y;
+	private final ModelPart leg2_z;
 	private final ModelPart seg2mid;
 	private final ModelPart seg2top;
 	private final ModelPart seg2bot;
-
+	private final ModelPart leg4_y;
+	private final ModelPart leg4_z;
+	private final ModelPart seg4mid;
+	private final ModelPart seg4top;
+	private final ModelPart seg4bot;
+	private final ModelPart leg1_y;
+	private final ModelPart leg1_z;
+	private final ModelPart seg1mid;
+	private final ModelPart seg1top;
+	private final ModelPart seg1bot;
+	private final ModelPart leg3_y;
+	private final ModelPart leg3_z;
+	private final ModelPart seg3mid;
+	private final ModelPart seg3top;
+	private final ModelPart seg3bot;
 
 
 	public TranscendentAsteronModel(ModelPart mp_root) {
 		this.root = mp_root.getChild("root");
-		this.face = this.root.getChild("face");
-		this.leg1 = this.root.getChild("leg1");
-		this.seg1mid = this.leg1.getChild("seg1mid");
-		this.seg1top = this.leg1.getChild("seg1top");
-		this.seg1bot = this.leg1.getChild("seg1bot");
-		this.leg3 = this.root.getChild("leg3");
-		this.seg3mid = this.leg3.getChild("seg3mid");
-		this.seg3top = this.leg3.getChild("seg3top");
-		this.seg3bot = this.leg3.getChild("seg3bot");
-		this.leg4 = this.root.getChild("leg4");
-		this.seg4mid = this.leg4.getChild("seg4mid");
-		this.seg4top = this.leg4.getChild("seg4top");
-		this.seg4bot = this.leg4.getChild("seg4bot");
-		this.leg2 = this.root.getChild("leg2");
-		this.seg2mid = this.leg2.getChild("seg2mid");
-		this.seg2top = this.leg2.getChild("seg2top");
-		this.seg2bot = this.leg2.getChild("seg2bot");
+		this.mainbody = this.root.getChild("mainbody");
+		this.leg2_y = this.root.getChild("leg2_y");
+		this.leg2_z = this.leg2_y.getChild("leg2_z");
+		this.seg2mid = this.leg2_z.getChild("seg2mid");
+		this.seg2top = this.leg2_z.getChild("seg2top");
+		this.seg2bot = this.leg2_z.getChild("seg2bot");
+		this.leg4_y = this.root.getChild("leg4_y");
+		this.leg4_z = this.leg4_y.getChild("leg4_z");
+		this.seg4mid = this.leg4_z.getChild("seg4mid");
+		this.seg4top = this.leg4_z.getChild("seg4top");
+		this.seg4bot = this.leg4_z.getChild("seg4bot");
+		this.leg1_y = this.root.getChild("leg1_y");
+		this.leg1_z = this.leg1_y.getChild("leg1_z");
+		this.seg1mid = this.leg1_z.getChild("seg1mid");
+		this.seg1top = this.leg1_z.getChild("seg1top");
+		this.seg1bot = this.leg1_z.getChild("seg1bot");
+		this.leg3_y = this.root.getChild("leg3_y");
+		this.leg3_z = this.leg3_y.getChild("leg3_z");
+		this.seg3mid = this.leg3_z.getChild("seg3mid");
+		this.seg3top = this.leg3_z.getChild("seg3top");
+		this.seg3bot = this.leg3_z.getChild("seg3bot");
 
 
 	}
@@ -61,57 +68,65 @@ public class TranscendentAsteronModel<T extends Entity> extends EntityModel<T> {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
-		PartDefinition root = partdefinition.addOrReplaceChild("root", CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -24.0F, -8.0F, 16.0F, 16.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+		PartDefinition root = partdefinition.addOrReplaceChild("root", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
 
-		PartDefinition face = root.addOrReplaceChild("face", CubeListBuilder.create().texOffs(16, 16).addBox(-8.0F, -8.0F, 1.99F, 16.0F, 16.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -16.0F, -10.0F));
+		PartDefinition mainbody = root.addOrReplaceChild("mainbody", CubeListBuilder.create().texOffs(0, 0).addBox(-14.0F, -16.0F, -2.0F, 16.0F, 16.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offset(6.0F, -6.8F, -6.0F));
 
-		PartDefinition leg1 = root.addOrReplaceChild("leg1", CubeListBuilder.create(), PartPose.offsetAndRotation(-1.15F, 0.0F, -4.0F, 0.0F, -0.7854F, 0.0F));
+		PartDefinition leg2_y = root.addOrReplaceChild("leg2_y", CubeListBuilder.create(), PartPose.offsetAndRotation(6.7F, -11.4F, -6.8F, 0.0F, 0.7854F, 0.0F));
 
-		PartDefinition seg1mid = leg1.addOrReplaceChild("seg1mid", CubeListBuilder.create().texOffs(10, 20).addBox(-2.0F, -7.2F, -2.0F, 4.0F, 8.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(17.0F, -5.0F, 2.0F));
+		PartDefinition leg2_z = leg2_y.addOrReplaceChild("leg2_z", CubeListBuilder.create(), PartPose.offsetAndRotation(-1.3F, 0.6F, 0.0F, 0.0F, 0.0F, -0.5236F));
 
-		PartDefinition seg1top = leg1.addOrReplaceChild("seg1top", CubeListBuilder.create(), PartPose.offset(17.0F, -9.0F, 2.0F));
+		PartDefinition seg2mid = leg2_z.addOrReplaceChild("seg2mid", CubeListBuilder.create().texOffs(10, 20).addBox(-2.0F, -7.2F, -2.0F, 4.0F, 8.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(5.1197F, 9.0F, -0.0263F));
 
-		PartDefinition seg1top_r1 = seg1top.addOrReplaceChild("seg1top_r1", CubeListBuilder.create().texOffs(10, 20).addBox(-1.0F, -6.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -0.6F, 0.0F, 0.0F, 0.0F, -0.829F));
+		PartDefinition seg2top = leg2_z.addOrReplaceChild("seg2top", CubeListBuilder.create(), PartPose.offset(7.1197F, 2.0F, -0.0263F));
 
-		PartDefinition seg1bot = leg1.addOrReplaceChild("seg1bot", CubeListBuilder.create(), PartPose.offset(13.0F, -1.0F, 2.0F));
+		PartDefinition seg2top_r1 = seg2top.addOrReplaceChild("seg2top_r1", CubeListBuilder.create().texOffs(10, 20).addBox(-1.0F, -6.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-2.0F, 2.0F, 0.05F, 0.0F, 0.0F, -0.829F));
 
-		PartDefinition seg1bot_r1 = seg1bot.addOrReplaceChild("seg1bot_r1", CubeListBuilder.create().texOffs(9, 20).addBox(-1.6F, -6.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -0.6F, 0.0F, 0.0F, 0.0F, 0.7854F));
+		PartDefinition seg2bot = leg2_z.addOrReplaceChild("seg2bot", CubeListBuilder.create(), PartPose.offsetAndRotation(7.1197F, 10.0F, -0.0263F, 0.0F, 0.0F, -0.2618F));
 
-		PartDefinition leg3 = root.addOrReplaceChild("leg3", CubeListBuilder.create(), PartPose.offsetAndRotation(3.9F, 0.0F, -1.1F, 0.0F, -2.3562F, 0.0F));
+		PartDefinition seg2bot_r1 = seg2bot.addOrReplaceChild("seg2bot_r1", CubeListBuilder.create().texOffs(9, 20).addBox(-1.6F, -6.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-6.0F, 2.4F, 0.0F, 0.0F, 0.0F, 0.7854F));
 
-		PartDefinition seg3mid = leg3.addOrReplaceChild("seg3mid", CubeListBuilder.create().texOffs(10, 20).addBox(-2.0F, -7.2F, -2.0F, 4.0F, 8.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(17.0F, -5.0F, 2.0F));
+		PartDefinition leg4_y = root.addOrReplaceChild("leg4_y", CubeListBuilder.create(), PartPose.offsetAndRotation(-6.8F, -11.4F, -6.8F, 0.0F, 2.3562F, 0.0F));
 
-		PartDefinition seg3top = leg3.addOrReplaceChild("seg3top", CubeListBuilder.create(), PartPose.offset(17.0F, -9.0F, 2.0F));
+		PartDefinition leg4_z = leg4_y.addOrReplaceChild("leg4_z", CubeListBuilder.create(), PartPose.offsetAndRotation(-1.3F, 0.6F, 0.0F, 0.0F, 0.0F, -0.5236F));
 
-		PartDefinition seg3top_r1 = seg3top.addOrReplaceChild("seg3top_r1", CubeListBuilder.create().texOffs(10, 20).addBox(-1.0F, -6.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -0.6F, 0.0F, 0.0F, 0.0F, -0.829F));
+		PartDefinition seg4mid = leg4_z.addOrReplaceChild("seg4mid", CubeListBuilder.create().texOffs(10, 20).addBox(-2.0F, -7.2F, -2.0F, 4.0F, 8.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(5.1197F, 9.0F, -0.0263F));
 
-		PartDefinition seg3bot = leg3.addOrReplaceChild("seg3bot", CubeListBuilder.create(), PartPose.offset(13.0F, -1.0F, 2.0F));
+		PartDefinition seg4top = leg4_z.addOrReplaceChild("seg4top", CubeListBuilder.create(), PartPose.offset(7.1197F, 2.0F, -0.0263F));
 
-		PartDefinition seg3bot_r1 = seg3bot.addOrReplaceChild("seg3bot_r1", CubeListBuilder.create().texOffs(10, 20).addBox(-1.6F, -6.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -0.6F, 0.0F, 0.0F, 0.0F, 0.7854F));
+		PartDefinition seg4top_r1 = seg4top.addOrReplaceChild("seg4top_r1", CubeListBuilder.create().texOffs(10, 20).addBox(-1.0F, -6.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-2.0F, 2.0F, 0.05F, 0.0F, 0.0F, -0.829F));
 
-		PartDefinition leg4 = root.addOrReplaceChild("leg4", CubeListBuilder.create(), PartPose.offsetAndRotation(1.15F, 0.0F, 4.05F, 0.0F, 2.3562F, 0.0F));
+		PartDefinition seg4bot = leg4_z.addOrReplaceChild("seg4bot", CubeListBuilder.create(), PartPose.offsetAndRotation(7.1197F, 10.0F, -0.0263F, 0.0F, 0.0F, -0.2618F));
 
-		PartDefinition seg4mid = leg4.addOrReplaceChild("seg4mid", CubeListBuilder.create().texOffs(10, 20).addBox(-2.0F, -7.2F, -2.0F, 4.0F, 8.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(17.0F, -5.0F, 2.0F));
+		PartDefinition seg4bot_r1 = seg4bot.addOrReplaceChild("seg4bot_r1", CubeListBuilder.create().texOffs(9, 20).addBox(-1.6F, -6.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-6.0F, 2.4F, 0.0F, 0.0F, 0.0F, 0.7854F));
 
-		PartDefinition seg4top = leg4.addOrReplaceChild("seg4top", CubeListBuilder.create(), PartPose.offset(17.0F, -9.0F, 2.0F));
+		PartDefinition leg1_y = root.addOrReplaceChild("leg1_y", CubeListBuilder.create(), PartPose.offsetAndRotation(6.7F, -11.4F, 6.7F, 0.0F, -0.7854F, 0.0F));
 
-		PartDefinition seg4top_r1 = seg4top.addOrReplaceChild("seg4top_r1", CubeListBuilder.create().texOffs(10, 20).addBox(-1.0F, -6.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -0.6F, 0.0F, 0.0F, 0.0F, -0.829F));
+		PartDefinition leg1_z = leg1_y.addOrReplaceChild("leg1_z", CubeListBuilder.create(), PartPose.offsetAndRotation(-1.3F, 0.6F, 0.0F, 0.0F, 0.0F, -0.5236F));
 
-		PartDefinition seg4bot = leg4.addOrReplaceChild("seg4bot", CubeListBuilder.create(), PartPose.offset(13.0F, -1.0F, 2.0F));
+		PartDefinition seg1mid = leg1_z.addOrReplaceChild("seg1mid", CubeListBuilder.create().texOffs(10, 20).addBox(-2.0F, -7.2F, -2.0F, 4.0F, 8.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(5.1197F, 9.0F, -0.0263F));
 
-		PartDefinition seg4bot_r1 = seg4bot.addOrReplaceChild("seg4bot_r1", CubeListBuilder.create().texOffs(10, 20).addBox(-1.6F, -6.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -0.6F, 0.0F, 0.0F, 0.0F, 0.7854F));
+		PartDefinition seg1top = leg1_z.addOrReplaceChild("seg1top", CubeListBuilder.create(), PartPose.offset(7.1197F, 2.0F, -0.0263F));
 
-		PartDefinition leg2 = root.addOrReplaceChild("leg2", CubeListBuilder.create(), PartPose.offsetAndRotation(-0.8F, 0.0F, -1.6F, 0.0F, 0.7854F, 0.0F));
+		PartDefinition seg1top_r1 = seg1top.addOrReplaceChild("seg1top_r1", CubeListBuilder.create().texOffs(10, 20).addBox(-1.0F, -6.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-2.0F, 2.0F, 0.05F, 0.0F, 0.0F, -0.829F));
 
-		PartDefinition seg2mid = leg2.addOrReplaceChild("seg2mid", CubeListBuilder.create().texOffs(10, 20).addBox(-2.0F, -7.2F, -2.0F, 4.0F, 8.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(12.8F, -5.0F, 1.6F));
+		PartDefinition seg1bot = leg1_z.addOrReplaceChild("seg1bot", CubeListBuilder.create(), PartPose.offsetAndRotation(7.1197F, 10.0F, -0.0263F, 0.0F, 0.0F, -0.2618F));
 
-		PartDefinition seg2top = leg2.addOrReplaceChild("seg2top", CubeListBuilder.create(), PartPose.offset(12.8F, -9.0F, 1.6F));
+		PartDefinition seg1bot_r1 = seg1bot.addOrReplaceChild("seg1bot_r1", CubeListBuilder.create().texOffs(9, 20).addBox(-1.6F, -6.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-6.0F, 2.4F, 0.0F, 0.0F, 0.0F, 0.7854F));
 
-		PartDefinition seg2top_r1 = seg2top.addOrReplaceChild("seg2top_r1", CubeListBuilder.create().texOffs(10, 20).addBox(-1.0F, -6.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -0.6F, 0.05F, 0.0F, 0.0F, -0.829F));
+		PartDefinition leg3_y = root.addOrReplaceChild("leg3_y", CubeListBuilder.create(), PartPose.offsetAndRotation(-6.8F, -11.4F, 6.7F, 0.0F, -2.3562F, 0.0F));
 
-		PartDefinition seg2bot = leg2.addOrReplaceChild("seg2bot", CubeListBuilder.create(), PartPose.offset(8.8F, -1.0F, 1.6F));
+		PartDefinition leg3_z = leg3_y.addOrReplaceChild("leg3_z", CubeListBuilder.create(), PartPose.offsetAndRotation(-1.3F, 0.6F, 0.0F, 0.0F, 0.0F, -0.5236F));
 
-		PartDefinition seg2bot_r1 = seg2bot.addOrReplaceChild("seg2bot_r1", CubeListBuilder.create().texOffs(9, 20).addBox(-1.6F, -6.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -0.6F, 0.0F, 0.0F, 0.0F, 0.7854F));
+		PartDefinition seg3mid = leg3_z.addOrReplaceChild("seg3mid", CubeListBuilder.create().texOffs(10, 20).addBox(-2.0F, -7.2F, -2.0F, 4.0F, 8.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(5.1197F, 9.0F, -0.0263F));
+
+		PartDefinition seg3top = leg3_z.addOrReplaceChild("seg3top", CubeListBuilder.create(), PartPose.offset(7.1197F, 2.0F, -0.0263F));
+
+		PartDefinition seg1top_r2 = seg3top.addOrReplaceChild("seg1top_r2", CubeListBuilder.create().texOffs(10, 20).addBox(-1.0F, -6.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-2.0F, 2.0F, 0.05F, 0.0F, 0.0F, -0.829F));
+
+		PartDefinition seg3bot = leg3_z.addOrReplaceChild("seg3bot", CubeListBuilder.create(), PartPose.offsetAndRotation(7.1197F, 10.0F, -0.0263F, 0.0F, 0.0F, -0.2618F));
+
+		PartDefinition seg1bot_r2 = seg3bot.addOrReplaceChild("seg1bot_r2", CubeListBuilder.create().texOffs(9, 20).addBox(-1.6F, -6.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-6.0F, 2.4F, 0.0F, 0.0F, 0.0F, 0.7854F));
 
 		return LayerDefinition.create(meshdefinition, 64, 64);
 	}
